1ef3a6d6e7175ea6ac8ed82e34ce8605b9944c4e
János Gönye <janos.gonye@gmail.com>
2021-10-18 14:49:58 +0200

    refactor home page tests

--- test/home/views/home_page_test.dart
+++ test/home/views/home_page_test.dart
@@ -5,24 +5,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:my_flutter_bloc_starter_project/home/views/home_page.dart';
 
 void main() {
+  Future<void> _pumpMaterialAppWithHomePage(WidgetTester tester) =>
+      tester.pumpWidget(const MaterialApp(home: HomePage()));
+
   group('HomePage', () {
-    testWidgets("displays an AppBar with title 'Home'",
+    testWidgets("displays an 'AppBar' with title 'Home'",
         (WidgetTester tester) async {
-      await tester.pumpWidget(const MaterialApp(home: HomePage()));
+      await _pumpMaterialAppWithHomePage(tester);
       expect(find.widgetWithText(AppBar, 'Home'), findsOneWidget);
     });
+  });
 
-    testWidgets(
-        "displays an 'ElevatedButton' with text 'Navigate to App Settings'",
-        (WidgetTester tester) async {
-      await tester.pumpWidget(const MaterialApp(home: HomePage()));
-      expect(
-        find.ancestor(
-          of: find.text('Navigate to App Settings'),
-          matching: find.byType(ElevatedButton),
-        ),
-        findsOneWidget,
+  group('HomePage/Navigate to App Settings Button', () {
+    Finder _findNavigatetoAppSettingsButton(WidgetTester tester) {
+      return find.ancestor(
+        of: find.text('Navigate to App Settings'),
+        matching: find.byType(ElevatedButton),
       );
+    }
+
+    testWidgets("is displayed with text 'Navigate to App Settings'",
+        (WidgetTester tester) async {
+      await _pumpMaterialAppWithHomePage(tester);
+      expect(_findNavigatetoAppSettingsButton(tester), findsOneWidget);
     });
   });
 }
