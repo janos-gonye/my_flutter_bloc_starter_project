562580126c492653d10528331107e6a3f715be53
János Gönye <janos.gonye@gmail.com>
2021-10-18 14:43:58 +0200

    add 'AppSettingsPage'

---
+++ lib/app_settings/app_settings.dart
@@ -0,0 +1,1 @@
+export 'views/app_settings_page.dart';
---
+++ lib/app_settings/views/app_settings_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class AppSettingsPage extends StatelessWidget {
+  const AppSettingsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('To be continued...'),
+    );
+  }
+}
