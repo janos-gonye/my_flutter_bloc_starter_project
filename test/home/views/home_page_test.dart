9257cb373a8c4bfed362bc755745474d4ca09961
János Gönye <janos.gonye@gmail.com>
2021-10-18 14:05:28 +0200

    add 'HomePage' with an 'AppBar' rendering a 'Text' with title 'Home'

---
+++ test/home/views/home_page_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_flutter_bloc_starter_project/home/views/home_page.dart';
+
+void main() {
+  group('HomePage', () {
+    testWidgets("has an AppBar", (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: HomePage()));
+      expect(find.byType(AppBar), findsOneWidget);
+    });
+  });
+  group('HomePage/AppBar', () {
+    testWidgets("has a title 'Home'", (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: HomePage()));
+      expect(find.widgetWithText(AppBar, 'Home'), findsOneWidget);
+    });
+  });
+}
