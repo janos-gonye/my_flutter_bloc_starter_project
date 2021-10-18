8caf052842eb2ea3b4ae64d933e3c3d3591ee3d3
János Gönye <janos.gonye@gmail.com>
2021-10-18 14:13:00 +0200

    merge 'HomePage' tests

--- test/home/views/home_page_test.dart
+++ test/home/views/home_page_test.dart
@@ -6,13 +6,7 @@ import 'package:my_flutter_bloc_starter_project/home/views/home_page.dart';
 
 void main() {
   group('HomePage', () {
-    testWidgets("has an AppBar", (WidgetTester tester) async {
-      await tester.pumpWidget(const MaterialApp(home: HomePage()));
-      expect(find.byType(AppBar), findsOneWidget);
-    });
-  });
-  group('HomePage/AppBar', () {
-    testWidgets("has a title 'Home'", (WidgetTester tester) async {
+    testWidgets("has an AppBar with title 'Home'", (WidgetTester tester) async {
       await tester.pumpWidget(const MaterialApp(home: HomePage()));
       expect(find.widgetWithText(AppBar, 'Home'), findsOneWidget);
     });
