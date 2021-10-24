426ae1fadad7e4fefb8945d22857d43f946b4d9c
János Gönye <janos.gonye@gmail.com>
2021-10-24 14:37:24 +0200

    move 'username' and 'password' models to 'shared/models'

--- lib/login/models/models.dart
+++ lib/login/models/models.dart
@@ -1,2 +1,2 @@
-export 'password.dart';
-export 'username.dart';
+export 'package:my_flutter_bloc_starter_project/shared/models/username.dart';
+export 'package:my_flutter_bloc_starter_project/shared/models/password.dart';
