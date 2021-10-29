b55be540dd594b1d6ba9eab4314953a2cf5fb990
János Gönye <janos.gonye@gmail.com>
2021-10-29 20:21:17 +0200

    add 'apiPathAuthRegistration' to 'constants.dart'

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,8 +2,10 @@ const String http = 'http';
 const String https = 'https';
 const List<String> protocols = [http, https];
 
-// as seconds
-const int connectionTimeout = 3000;
-// as seconds
-const int receiveTimeout = 5000;
+// as milliseconds
+const int connectionTimeout = 10000;
+// as milliseconds
+const int receiveTimeout = 3000;
 const contentType = 'application/json';
+
+const apiPathAuthRegistration = '/auth/registration/';
