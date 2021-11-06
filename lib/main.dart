035ce3cd6520e14b626a9f8a39a7bc9b100f0773
János Gönye <janos.gonye@gmail.com>
2021-11-06 12:37:33 +0100

    apply 'pretty_do_logger' to add http logging

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 import 'package:my_flutter_bloc_starter_project/app.dart';
 import 'package:my_flutter_bloc_starter_project/app_settings/app_settings.dart';
@@ -32,5 +33,18 @@ Dio initDio() {
   _dio.options.connectTimeout = constants.connectionTimeout;
   _dio.options.receiveTimeout = constants.receiveTimeout;
   _dio.options.contentType = constants.contentType;
+  _dio.interceptors.add(initPrettyDioLogger());
   return _dio;
+}
+
+PrettyDioLogger initPrettyDioLogger() {
+  return PrettyDioLogger(
+    requestHeader: true,
+    requestBody: true,
+    responseBody: true,
+    responseHeader: false,
+    error: true,
+    compact: true,
+    maxWidth: 90,
+  );
 }
