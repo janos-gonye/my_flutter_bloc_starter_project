5d6cec54e8aa6bf1b78de089c89f72cba5c434d4
János Gönye <janos.gonye@gmail.com>
2021-11-16 13:48:15 +0100

    pop until route is different on 'NotFoundPage'

--- lib/not_found/views/not_found_page.dart
+++ lib/not_found/views/not_found_page.dart
@@ -8,18 +8,28 @@ class NotFoundPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Not Found'),
-      ),
-      body: const Center(
-        child: SingleChildScrollView(
-          child: Text(
-            '404?!',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.bold,
+    return WillPopScope(
+      onWillPop: () async {
+        // Override default `pop` functionality -> Only matters in browsers.
+        // Even if the user navigates to many unknown routes, the back button
+        //only needs to be pushed once to return to the last known route.
+        Navigator.of(context)
+            .popUntil((route) => route.settings.name != routeName);
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Not Found'),
+        ),
+        body: const Center(
+          child: SingleChildScrollView(
+            child: Text(
+              '404?!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
